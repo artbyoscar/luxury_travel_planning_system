e1f30c6c4d30964899cbd5c59d04a36d4d67fb82
ONunez <ONunez@ecpi.edu>
2024-10-02 11:09:58 -0700

    updates

--- frontend/src/firebaseConfig.ts
+++ frontend/src/firebaseConfig.ts
@@ -1,14 +1,14 @@
 // frontend/src/firebaseConfig.ts
 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 // Import other Firebase services as needed, e.g., getStorage
 
-// Load environment variables
+// Firebase configuration from environment variables
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY, // Ensure prefix REACT_APP_
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
@@ -21,10 +21,14 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase services
-const analytics = getAnalytics(app);
+let analytics;
+isSupported().then((supported) => {
+  if (supported) {
+    analytics = getAnalytics(app);
+  }
+});
 const db = getFirestore(app); // Firestore
 const auth = getAuth(app);     // Authentication
 // Initialize other services if needed, e.g., const storage = getStorage(app);
 
-// Export initialized services for use in your app
 export { app, analytics, db, auth /*, storage */ };
