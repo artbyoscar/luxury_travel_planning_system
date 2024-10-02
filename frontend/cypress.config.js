06c7068cde1e40c01cd65d7453e0b7f40534d88f
ONunez <ONunez@ecpi.edu>
2024-10-02 14:18:59 -0700

    cypress and firebase connection?

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -12,12 +12,12 @@ module.exports = defineConfig({
     },
   },
   env: {
-    REACT_APP_FIREBASE_API_KEY: 'your-api-key',
-    REACT_APP_FIREBASE_AUTH_DOMAIN: 'your-auth-domain',
-    REACT_APP_FIREBASE_PROJECT_ID: 'your-project-id',
-    REACT_APP_FIREBASE_STORAGE_BUCKET: 'your-storage-bucket',
-    REACT_APP_FIREBASE_MESSAGING_SENDER_ID: 'your-messaging-sender-id',
-    REACT_APP_FIREBASE_APP_ID: 'your-app-id',
-    REACT_APP_FIREBASE_MEASUREMENT_ID: 'your-measurement-id'
+    REACT_APP_FIREBASE_API_KEY: 'AIzaSyDlgw_1pSa0YbfsNRNDcG6IhS7QZ3NgMqY',
+    REACT_APP_FIREBASE_AUTH_DOMAIN: 'luxurytravelplanningsystem.firebaseapp.com',
+    REACT_APP_FIREBASE_PROJECT_ID: 'luxurytravelplanningsystem',
+    REACT_APP_FIREBASE_STORAGE_BUCKET: 'luxurytravelplanningsystem.appspot.com',
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID: '106801283144',
+    REACT_APP_FIREBASE_APP_ID: '1:106801283144:web:8a5a84fef8119b0ff7187d',
+    REACT_APP_FIREBASE_MEASUREMENT_ID: 'G-EQM5PW6Q44'
   }
 });
